e51919f38ed35b2ebad83fe66c77c61e6205af4d
ArcherWn <mhqjeaock0649@gmail.com>
2024-04-03 17:26:14 +0800

    Remove commented out code for SelectPrimitive.Icon

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -25,9 +25,9 @@ const SelectTrigger = React.forwardRef<
         {...props}
     >
         {children}
-        <SelectPrimitive.Icon asChild>
+        {/* <SelectPrimitive.Icon asChild>
             <ChevronDown className="h-4 w-4 opacity-50" />
-        </SelectPrimitive.Icon>
+        </SelectPrimitive.Icon> */}
     </SelectPrimitive.Trigger>
 ));
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
