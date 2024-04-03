cdc5023135c8959c2f452b0d787a6d5db31aa37b
ArcherWn <mhqjeaock0649@gmail.com>
2024-04-03 21:32:35 +0800

    remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
