a2a12b2fec5058d355dda2dc11fc50d4d49198c6
ArcherWn <mhqjeaock0649@gmail.com>
2024-04-04 19:53:14 +0800

    Refactor styling in Letters component

--- src/app/[locale]/puzzle/(convert)/letters/letters.tsx
+++ src/app/[locale]/puzzle/(convert)/letters/letters.tsx
@@ -17,10 +17,9 @@ export default function Letters() {
     return (
         <div className="flex flex-col">
             <div className="flex flex-col items-center p-5">
-                <div className="w-full lg:px-24">
+                <div className="w-full sm:px-12 xl:px-24">
                     <Label className="text-xl">Number</Label>
                     <Input
-                        size={60}
                         type="text"
                         value={number}
                         onChange={(e) => setNumber(e.target.value)}
@@ -28,10 +27,10 @@ export default function Letters() {
                     />
                 </div>
                 <ArrowLeftRightIcon size={40} className="mt-7 rotate-90" />
-                <div className="p-3 text-7xl lg:text-9xl">{letters.join(' ')}</div>
+                <div className="p-3 text-7xl sm:text-9xl">{letters.join(' ')}</div>
             </div>
 
-            <div className="grid grid-flow-col grid-rows-9 place-items-center gap-x-12 gap-y-4 p-6 pt-2 text-2xl lg:grid-rows-8 lg:gap-y-6 lg:pt-10">
+            <div className="grid grid-flow-col grid-rows-9 place-items-center gap-x-12 gap-y-4 p-6 pt-2 text-2xl sm:grid-rows-8 xl:gap-y-6 xl:pt-10">
                 {Array.from({ length: 26 }).map((_, index) => (
                     <div key={index} className="grid grid-cols-3 place-items-center gap-2">
                         <span>{(index + 1).toString().padStart(2, '0')}</span>
