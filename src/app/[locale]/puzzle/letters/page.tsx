b9b1092a6ac2c0dc423d026f01b5fe015b437bc9
Archer_Wn <mhqjeaock0649@gmail.com>
2024-04-08 11:48:16 +0800

    Update Morse, Letters, and Binary puzzle pages with metadata generation and localization support

--- src/app/[locale]/puzzle/letters/page.tsx
+++ src/app/[locale]/puzzle/letters/page.tsx
@@ -1,5 +1,39 @@
+import { Metadata } from 'next';
+import { useTranslations } from 'next-intl';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import BreadcrumbNav from '@/components/breadcrumb-nav';
+
 import Letters from './letters';
 
-export default function Page() {
-    return <Letters />;
+type Props = {
+    params: { locale: string };
+};
+
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }): Promise<Metadata> {
+    const t = await getTranslations({ locale, namespace: 'LettersPage' });
+
+    return {
+        title: t('title'),
+        description: t('description'),
+    };
+}
+
+export default function LettersPage({ params: { locale } }: Props) {
+    // Enable static rendering
+    unstable_setRequestLocale(locale);
+
+    const t = useTranslations('LettersPage');
+
+    return (
+        <>
+            <div>
+                <h1 className="text-4xl font-semibold">{t('title')}</h1>
+                <BreadcrumbNav className="mt-2" />
+            </div>
+            <div className="mt-2 grow">
+                <Letters />
+            </div>
+        </>
+    );
 }
--- src/app/[locale]/puzzle/morse/morse.tsx
+++ src/app/[locale]/puzzle/morse/morse.tsx
@@ -64,53 +64,47 @@ export default function Morse() {
     });
 
     return (
-        <>
-            <div>
-                <h1 className="text-4xl font-semibold">{t('title')}</h1>
-                <BreadcrumbNav className="mt-2" />
+        <div className="flex flex-col">
+            <div className="flex flex-col items-center p-5">
+                <div className="w-full sm:px-10 xl:px-20">
+                    <Label className="text-xl">Morse Code</Label>
+                    <Input
+                        size={60}
+                        type="text"
+                        value={morse}
+                        onChange={(e) => setMorse(e.target.value)}
+                        placeholder=".- -... -.-."
+                    />
+                </div>
+                <ArrowLeftRightIcon size={40} className="mt-7 rotate-90" />
+                <div className="p-3 text-7xl sm:text-9xl">{chars.join(' ')}</div>
             </div>
-            <div className="flex flex-col">
-                <div className="flex flex-col items-center p-5">
-                    <div className="w-full sm:px-10 xl:px-20">
-                        <Label className="text-xl">Morse Code</Label>
-                        <Input
-                            size={60}
-                            type="text"
-                            value={morse}
-                            onChange={(e) => setMorse(e.target.value)}
-                            placeholder=".- -... -.-."
-                        />
-                    </div>
-                    <ArrowLeftRightIcon size={40} className="mt-7 rotate-90" />
-                    <div className="p-3 text-7xl sm:text-9xl">{chars.join(' ')}</div>
+
+            <div className="flex flex-col items-center xl:flex-row xl:justify-center">
+                <div className="grid grid-flow-col grid-rows-5 place-items-center gap-x-12 gap-y-4 p-6 pt-2 sm:text-2xl xl:pt-10">
+                    {Object.entries(numberMorsetMap).map(([char, morse]) => (
+                        <div key={char} className="grid grid-cols-5 place-items-center gap-2">
+                            <span>{char}</span>
+                            <ArrowRightIcon size={20} className="w-10" />
+                            <span className="col-span-3 text-xs sm:text-2xl">
+                                {morse.replace(/\-/g, '━').replace(/\./g, '•').split('').join(' ')}
+                            </span>
+                        </div>
+                    ))}
                 </div>
 
-                <div className="flex flex-col items-center xl:flex-row xl:justify-center">
-                    <div className="grid grid-flow-col grid-rows-5 place-items-center gap-x-12 gap-y-4 p-6 pt-2 sm:text-2xl xl:pt-10">
-                        {Object.entries(numberMorsetMap).map(([char, morse]) => (
-                            <div key={char} className="grid grid-cols-5 place-items-center gap-2">
-                                <span>{char}</span>
-                                <ArrowRightIcon size={20} className="w-10" />
-                                <span className="col-span-3 text-xs sm:text-2xl">
-                                    {morse.replace(/\-/g, '━').replace(/\./g, '•').split('').join(' ')}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className="grid grid-flow-col grid-rows-13 place-items-center gap-x-12 gap-y-4 p-6 pt-2 sm:grid-rows-9 sm:text-2xl xl:grid-rows-9 xl:pt-10">
-                        {Object.entries(LetterMorseMap).map(([char, morse]) => (
-                            <div key={char} className="grid grid-cols-5 place-items-center gap-2">
-                                <span>{char}</span>
-                                <ArrowRightIcon size={20} className="w-10" />
-                                <span className="col-span-3 text-xs sm:text-2xl">
-                                    {morse.replace(/\-/g, '━').replace(/\./g, '•').split('').join(' ')}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
+                <div className="grid grid-flow-col grid-rows-13 place-items-center gap-x-12 gap-y-4 p-6 pt-2 sm:grid-rows-9 sm:text-2xl xl:grid-rows-9 xl:pt-10">
+                    {Object.entries(LetterMorseMap).map(([char, morse]) => (
+                        <div key={char} className="grid grid-cols-5 place-items-center gap-2">
+                            <span>{char}</span>
+                            <ArrowRightIcon size={20} className="w-10" />
+                            <span className="col-span-3 text-xs sm:text-2xl">
+                                {morse.replace(/\-/g, '━').replace(/\./g, '•').split('').join(' ')}
+                            </span>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
