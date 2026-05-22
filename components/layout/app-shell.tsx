b613fd3432e19f866a7a04dadbdc644b198e36a0
Archer_Wn <mhqjeaock0649@gmail.com>
2026-05-22 19:57:39 +0800

    chore: add background gradient to AppShell for improved visual design

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -6,7 +6,8 @@ import { AppBreadcrumbs } from './app-breadcrumbs';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col">
+      <div className="absolute -z-10 h-full w-full bg-[radial-gradient(var(--muted)_1px,transparent_1px)] mask-[radial-gradient(ellipse_50%_50%_at_50%_50%,var(--background)_70%,transparent_100%)] bg-size-[16px_16px]"></div>
       <Header />
       <AppBreadcrumbs />
 
